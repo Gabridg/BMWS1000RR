0695ab55cf98d79827d4b26542b3b6fbca2dda9f
Gabridg <gabri99.del@gmail.com>
2024-05-15 09:35:19 +0200

    fisrt commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="icon" href="{{asset('images/EBAFoS-no-bg.png')}}">
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- -- My Style & Script -- -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <title>Crowfounding | BMW S1000RR</title>
+</head>
+<body>
+    <header>
+        <div></div>
+        <nav>
+            <div>Lingua</div>
+            <div>Donare</div>
+            <div>Regole</div>
+        </nav>
+    </header>
+
+    <main>
+        
+    </main>
+</body>
+</html>
